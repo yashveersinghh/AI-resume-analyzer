6ebce669963e562ccfc28fe20f2f8746cfeb3bea
Yashveer singh <yashveersingh1921@gmail.com>
2026-07-19 23:54:05 +0530

    implemented auth routers

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -1,8 +1,109 @@
-async function registerUserController(req, res){
-    
+const userModel = require("../models/user.model");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+/**
+ * @name registerUserController
+ * @description Register a new user, expects username, email and password in the request body
+ * @access Public
+ */
+async function registerUserController(req, res) {
+  const { username, email, password } = req.body;
+
+  if (!username || !email || !password) {
+    return res.status(400).json({
+      message: "Please provide username, email and password",
+    });
+  }
+
+  const isUserAlreadyExists = userModel.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  if (isUserAlreadyExists.username == username) {
+    return res.status(400).json({
+      message: "User already exists with this username",
+    });
+  } else {
+    return res.status(400).json({
+      message: "User already exists with this email address",
+    });
+  }
+
+  const hash = await bcrypt.hash(password, 10);
+
+  const user = await userModel.create({
+    username,
+    email,
+    password: hash,
+  });
+
+  const token = jwt.sign(
+    { id: user._id, username: user.username },
+    process.env.JWT_SECRET,
+    { expiresIn: "1d" },
+  );
+
+  res.cookie("token", token);
+
+  res.status(201).json({
+    message: "User created successfully",
+    user: {
+      id: user._id,
+      username: user.username,
+      email: user.email,
+    },
+  });
 }
 
+/**
+ * @name loginUserController
+ * @description login a user, expects email and password in the request body
+ * @access public
+ */
+async function loginUserController(res, req) {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({
+      message: "Please provide email address and password",
+    });
+  }
+
+  const user = await userModel.findOne({ email });
+
+  if (!user) {
+    return res.status(400).json({
+      message: "Invalid email or password",
+    });
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.passsword);
+
+  if (!isPasswordValid) {
+    return res.status(400).json({
+      message: "Inavalid email or password",
+    });
+  }
+
+  const token = jwt.sign(
+    { id: user._id, username: user.username },
+    process.env.JWT_SECRET,
+    { expiresIn: "id" },
+  );
+
+  res.cookie("token", token);
+  res.status(200).json({
+    message: "user loggedIn successfully",
+    user: {
+      id: user._id,
+      username: user.username,
+      email: user.email,
+    },
+  });
+}
 
 module.exports = {
-    registerUserController
-}
+  registerUserController,
+  loginUserController
+};
--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -3,6 +3,18 @@ const authController = require("../controllers/auth.controller");
 
 const authRouter = express.Router();
 
-authRouter.post("/register", authController.registerUserController)
+/**
+ * @route POST /api/auth/register
+ * @description Register a new user
+ * @access Public
+ */
+authRouter.post("/register", authController.registerUserController);
+
+/**
+ * @route POST /api/auth/login
+ * @description Login user with email and password
+ * @access Public
+ */
+authRouter.post("/login", authController.loginUserController);
 
 module.exports = authRouter;
