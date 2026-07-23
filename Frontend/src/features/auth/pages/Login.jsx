16b0148eee2006af3fae37765ee85f20295c4da7
Yashveer singh <yashveersingh1921@gmail.com>
2026-07-24 00:45:35 +0530

    auth ui implemented

--- Frontend/src/features/auth/pages/Login.jsx
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -1,25 +1,42 @@
-import "../auth.form.scss"
+import { Link, useNavigate } from "react-router";
+import "../auth.form.scss";
 
 const Login = () => {
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <main>
       <div className="form-container">
         <h1>Login</h1>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="input-group">
             <label htmlFor="email">Email</label>
-            <input type="email" name="email" id="email" placeholder="Enter your email address" />
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Enter your email address"
+            />
           </div>
           <div className="input-group">
             <label htmlFor="password">Password</label>
-            <input type="password" name="password" id="password" placeholder="Enter your password" />
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Enter your password"
+            />
           </div>
           <button className="button primary-button ">Login</button>
         </form>
+        <p>Don't have an account? <Link to={"/register"}>Register</Link></p>
       </div>
     </main>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- Frontend/src/features/auth/pages/Register.jsx
+++ Frontend/src/features/auth/pages/Register.jsx
@@ -1,7 +1,52 @@
-const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+import "../auth.form.scss";
+import { Link, useNavigate } from "react-router";
 
-export default Register
+const Register = () => {
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <main>
+      <div className="form-container">
+        <h1>Register</h1>
+
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <label htmlFor="username">Username</label>
+            <input
+              type="username"
+              name="username"
+              id="username"
+              placeholder="Enter your username"
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Enter your email address"
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Enter your password"
+            />
+          </div>
+          <button className="button primary-button ">Register</button>
+        </form>
+        <p>Already have an account? <Link to={"/login"}>Login</Link></p>
+      </div>
+    </main>
+  );
+};
+
+export default Register;
